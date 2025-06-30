ab39ae70fb87c1001f0f806c476919202bf9a0f1
Saksham Tolani <saksham@quoriumconsulting.com>
2025-06-30 17:11:06 +0530

    final push

---
+++ src/App.tsx
@@ -0,0 +1,25 @@
+import Navbar from './components/Navbar';
+import Hero from './components/Hero';
+import About from './components/About';
+import Services from './components/Services';
+import Testimonials from './components/Testimonials';
+import Contact from './components/Contact';
+import Footer from './components/Footer';
+
+function App() {
+  return (
+    <div className="min-h-screen bg-navy-900">
+      <Navbar />
+      <main>
+        <Hero />
+        <About />
+        <Services />
+        <Testimonials />
+        <Contact />
+      </main>
+      <Footer />
+    </div>
+  );
+}
+
+export default App;
